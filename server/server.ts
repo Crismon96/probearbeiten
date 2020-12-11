35bbdf288a1b5838c4e412cdcab4b2cbc44bb566
Christoph Griehl <christoph.griehl@kiwigrid.com>
2020-12-11 21:00:32 +0100

    add proxy and fallback REST endpoint

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { graphqlHTTP } from "express-graphql";
+import { graphqlHTTP } from 'express-graphql';
 import { makeExecutableSchema } from 'graphql-tools';
 import mutationResolver from './resolver/mutationResolver';
 import objectTypeResolver from './resolver/objectTypeResolver';
@@ -8,16 +8,29 @@ import typeDefs from './schema/rootSchema';
 
 const app = express();
 
+// For REST just in case I need it
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 const executableSchema = makeExecutableSchema({
   typeDefs,
   resolvers: [queryResolver, mutationResolver, objectTypeResolver],
 });
 
-app.use('/graphql', graphqlHTTP({
-  schema: executableSchema,
-  graphiql: true,
-}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: executableSchema,
+    graphiql: true,
+  })
+);
 
 app.listen(8000, '127.0.0.1', () => {
-    console.log("Listening")
-})
+  console.log('Listening to port 8000');
+});
